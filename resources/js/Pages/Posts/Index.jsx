f526fed71a9284d1984059c93344b56c7483c635
Emman <click2edit@gmail.com>
2024-06-20 04:25:10 +0800

    create page needs to be modified

--- resources/js/Pages/Posts/Index.jsx
+++ resources/js/Pages/Posts/Index.jsx
@@ -10,14 +10,19 @@ const Index = ({ posts }) => {
     };
 
     return (
-        <div>
-            <h1>Posts</h1>
-            <InertiaLink href="/posts/create">Create Post</InertiaLink>
-            <ul>
+        <div class="text-center">
+            <br />
+<h1 class="text-5xl font-extrabold dark:text-white">Posts</h1>
+
+<br />
+            
+            
+            <InertiaLink class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" href="/posts/create">Create Post</InertiaLink>
+            <ul class="mt-12">
                 {posts.map(post => (
                     <li key={post.id}>
-                        <InertiaLink href={`/posts/${post.id}/edit`}>{post.title}</InertiaLink>
-                        <button onClick={() => handleDelete(post.id)}>Delete</button>
+                        <InertiaLink class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 uppercase" href={`/posts/${post.id}/edit`}>{post.title}</InertiaLink>
+                        <button onClick={() => handleDelete(post.id)} class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Delete</button>
                     </li>
                 ))}
             </ul>
